8c3769668e424e956939ca6a79efdc816a1decab
Anthony Sabbagh <ash@f4-group.com>
2022-01-12 16:25:10 +0100

    add: prototype

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+Vue.prototype.$foo = 'foo';
+Vue.prototype.$myFunction = (a, b) => a + b;
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
